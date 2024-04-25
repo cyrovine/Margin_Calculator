85b3533b5bc39680726a0b699578b1412e482e48
洪呈翰 <79899783+cyrovine@users.noreply.github.com>
2024-04-25 11:15:19 +0800

    Create futures_deposit_set.py
    保證金AB值

---
+++ futures_deposit_set.py
@@ -0,0 +1,171 @@
+def find_deposit(prod):
+    match prod:
+        case "TX":
+            balance=132000
+            keep=137000
+            raw=179000
+            return(balance,keep,raw)
+        
+        case "MTX":
+            balance=33000
+            keep=34250
+            raw=44750
+            return(balance,keep,raw)
+        
+        case "MXFFX":
+            balance=33000
+            keep=34250
+            raw=44750
+            return(balance,keep,raw)
+        
+        case "TE":
+            balance=152000
+            keep=158000
+            raw=206000
+            return(balance,keep,raw)
+        
+        case "ZEF":
+            balance=19000
+            keep=19750
+            raw=25750
+            return(balance,keep,raw)
+        
+        case "TF":
+            balance=58000
+            keep=61000
+            raw=79000
+            return(balance,keep,raw)
+        
+        case "ZFF":
+            balance=14500
+            keep=15250
+            raw=19750
+            return(balance,keep,raw)
+        
+        case "XIF":
+            balance=46000
+            keep=48000
+            raw=63000
+            return(balance,keep,raw)
+        
+        case "GTF":
+            balance=35000
+            keep=37000
+            raw=48000
+            return(balance,keep,raw)
+        
+        case "G2F":
+            balance=18000
+            keep=19000
+            raw=25000
+            return(balance,keep,raw)
+        
+        case "E4F":
+            balance=31000
+            keep=33000
+            raw=42000
+            return(balance,keep,raw)
+        
+        case "BTF":
+            balance=11000
+            keep=12000
+            raw=15000
+            return(balance,keep,raw)
+        
+        case "SOF":
+            balance=13000
+            keep=14000
+            raw=18000
+            return(balance,keep,raw)
+        
+        case "SHF":
+            balance=10000
+            keep=11000
+            raw=14000
+            return(balance,keep,raw)
+        
+        case "TJF":
+            balance=17000
+            keep=18000
+            raw=23000
+            return(balance,keep,raw)
+        
+        case "UDF":
+            balance=28000
+            keep=29000
+            raw=38000
+            return(balance,keep,raw)
+        
+        case "SPF":
+            balance=42000
+            keep=44000
+            raw=57000
+            return(balance,keep,raw)
+        
+        case "UNF":
+            balance=37000
+            keep=39000
+            raw=50000
+            return(balance,keep,raw)
+        
+        case "SXF":
+            balance=25000
+            keep=26000
+            raw=34000
+            return(balance,keep,raw)
+        
+        case "F1F":
+            balance=20000
+            keep=21000
+            raw=27000
+            return(balance,keep,raw)
+        case "GDF":
+            balance=900	
+            keep=940	
+            raw=1220
+            return(balance,keep,raw)
+        case "TGF":
+            balance=34000
+            keep=36000
+            raw=46000
+            return(balance,keep,raw)
+        case "BRF":
+            balance=48000
+            keep=50000
+            raw=65000
+            return(balance,keep,raw)
+        case "RTF":
+            balance=2200
+            keep=2280
+            raw=2970
+            return(balance,keep,raw)
+        case "RHF":
+            balance=10800
+            keep=11180
+            raw=14580
+            return(balance,keep,raw)
+        
+        case "XEF":
+            balance=440	
+            keep=460
+            raw=600
+            return(balance,keep,raw)
+        
+        case "XJF":
+            balance=84,000
+            keep=87000
+            raw=114000
+            return(balance,keep,raw)
+        
+        case "XBF":
+            balance=640
+            keep=670
+            raw=870
+            return(balance,keep,raw)
+        
+        case "XAF":
+            balance=480
+            keep=500	
+            raw=650
+            return(balance,keep,raw)
+        
